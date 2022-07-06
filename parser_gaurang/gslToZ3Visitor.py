34dd823a67ce6b97c09efb73edcafc1ef70ffce1
Gaurang  Bajaj <gaurangbajaj.gb@gmail.com>
2022-07-07 02:36:01 +0530

    Exxtended visitor

--- parser_gaurang/gslToZ3Visitor.py
+++ parser_gaurang/gslToZ3Visitor.py
@@ -10,24 +10,42 @@ class gslToZ3Visitor(gslVisitor):
 
     # Visit a parse tree produced by gslParser#decl.
     def visitDecl(self, ctx:gslParser.DeclContext):
-        # print(ctx.getText())
-        self.symbolTable.append(str(ctx.IDENTIFIER()))
-        # self.printSymbolTable()
+        print(ctx.getText())
+        type = self.visit(ctx.type_())
+        variable = str(ctx.IDENTIFIER())
+        self.symbolTable.append((variable, type))
+        self.printSymbolTable()
         return self.visitChildren(ctx)
+
+    # Visit a parse tree produced by gslParser#type.
+    def visitType(self, ctx:gslParser.TypeContext):
+        return str(ctx.children[0])
     
     # Visit a parse tree produced by gslParser#assignment.
     def visitAssignment(self, ctx:gslParser.AssignmentContext):
         print(ctx.getText())
         # print(self.symbolTable[0])
-        if str(ctx.IDENTIFIER()) not in self.symbolTable:
+        varPresent = False
+        for symbol in self.symbolTable:
+            print(symbol)
+            if str(ctx.IDENTIFIER()) in symbol[0]:
+                varPresent = True
+        if(not varPresent):
             print("Error: Identifier not declared")
         return self.visitChildren(ctx)
 
     # Visit a parse tree produced by gslParser#expr.
     def visitExpr(self, ctx:gslParser.ExprContext):
         print(ctx.getText())
-        if ctx.IDENTIFIER() and str(ctx.IDENTIFIER()) not in self.symbolTable:
-            print("Error: Identifier not declared")
+
+        varPresent = False
+        if ctx.IDENTIFIER():
+            for symbol in self.symbolTable:
+                print(symbol)
+                if str(ctx.IDENTIFIER()) in symbol[0]:
+                    varPresent = True
+            if(not varPresent):
+                print("Error: Identifier not declared")
         return self.visitChildren(ctx)
 
     def printSymbolTable(self):
