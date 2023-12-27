805614b176079cfaf0ddca1b5fbd7703cc683c9a
Stanly Samuel <stanly@iisc.ac.in>
2023-12-27 22:26:44 +0530

    Updated gitignore and setup

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='gensys',
-    version='0.1.0',
+    name='gensys-ltl',
+    version='0.2.0',
     packages=find_packages(include=['gensys', 'gensys.*']),
     install_requires=[
         'z3-solver'
